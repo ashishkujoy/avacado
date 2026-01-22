868b323cd64e03493c979ff810ffd0ebfd580682
ashishku <ashishkujoy@gmail.com>
2026-01-22 20:01:44 +0530

    Adds expiry details in kv value

--- internal/storage/kv/memory/memory.go
+++ internal/storage/kv/memory/memory.go
@@ -5,21 +5,34 @@ import (
 	"context"
 	"fmt"
 	"sync"
+	"time"
 )
 
+type value struct {
+	data   []byte
+	expiry *time.Time
+}
+
+func (v *value) isExpired() bool {
+	if v.expiry == nil {
+		return false
+	}
+	return time.Now().After(*v.expiry)
+}
+
 type KVMemoryStore struct {
-	store map[string][]byte
+	store map[string]*value
 	mu    sync.RWMutex
 }
 
 func NewKVMemoryStore() *KVMemoryStore {
 	return &KVMemoryStore{
-		store: make(map[string][]byte),
+		store: make(map[string]*value),
 		mu:    sync.RWMutex{},
 	}
 }
 
-func (k *KVMemoryStore) Set(_ context.Context, key string, value []byte, options *kv.SetOptions) error {
+func (k *KVMemoryStore) Set(_ context.Context, key string, data []byte, options *kv.SetOptions) error {
 	k.mu.Lock()
 	defer k.mu.Unlock()
 	_, keyAlreadyExists := k.store[key]
@@ -29,14 +42,17 @@ func (k *KVMemoryStore) Set(_ context.Context, key string, value []byte, options
 	if !keyAlreadyExists && options.XX {
 		return NewKeyNotPresentError(key)
 	}
-	k.store[key] = value
+	k.store[key] = &value{data: data, expiry: nil}
 	return nil
 }
 
 func (k *KVMemoryStore) Get(_ context.Context, key string) ([]byte, error) {
 	k.mu.RLock()
 	defer k.mu.RUnlock()
-	return k.store[key], nil
+	if value, ok := k.store[key]; ok {
+		return value.data, nil
+	}
+	return nil, nil
 }
 
 func NewKeyAlreadyExistsError(key string) error {
