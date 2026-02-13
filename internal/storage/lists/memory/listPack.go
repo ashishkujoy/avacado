cd33abe2ee8ce1ec1ecb988c81d035b988901ddb
ashishku <ashishkujoy@gmail.com>
2026-02-13 14:14:16 +0530

    Adds listPack

---
+++ internal/storage/lists/memory/listPack.go
@@ -0,0 +1,89 @@
+package memory
+
+import (
+	"encoding/binary"
+	"sync"
+)
+
+// listPack represents a list pack data structure used for storing small lists in memory.
+type listPack struct {
+	mu   sync.RWMutex
+	data []byte
+}
+
+func newEmptyListPack() *listPack {
+	data := make([]byte, 1024)
+	binary.BigEndian.PutUint32(data[:4], 7)
+	binary.BigEndian.PutUint16(data[4:6], 0)
+	data[6] = 0xFF
+	return &listPack{data: data, mu: sync.RWMutex{}}
+}
+
+func newListPack(elements ...[]byte) *listPack {
+	lp := newEmptyListPack()
+	lp.byteSize()
+	lp.push(elements...)
+	return lp
+}
+
+func (lp *listPack) length() int {
+	lp.mu.RLock()
+	defer lp.mu.RUnlock()
+	return int(binary.BigEndian.Uint16(lp.data[4:6]))
+}
+
+func (lp *listPack) byteSize() int {
+	lp.mu.RLock()
+	defer lp.mu.RUnlock()
+	return int(binary.BigEndian.Uint32(lp.data[:4]))
+}
+
+func (lp *listPack) push(values ...[]byte) int {
+	lp.mu.Lock()
+	defer lp.mu.Unlock()
+	elemCount := int(binary.BigEndian.Uint16(lp.data[4:6]))
+	newElemCount := elemCount + len(values)
+
+	offset := int(binary.BigEndian.Uint32(lp.data[:4])) - 1
+	for _, value := range values {
+		offset = lp.encodeElement(lp.data, offset, value)
+	}
+	lp.data[offset] = 0xFF
+	offset += 1
+	binary.BigEndian.PutUint32(lp.data[:4], uint32(offset))
+	binary.BigEndian.PutUint16(lp.data[4:6], uint16(newElemCount))
+	return newElemCount
+}
+
+func (lp *listPack) encodeElement(buf []byte, offset int, element []byte) int {
+	binary.BigEndian.PutUint16(buf[offset:offset+2], uint16(len(element)))
+	offset += 2
+	copy(buf[offset:offset+len(element)], element)
+	offset += len(element)
+	binary.BigEndian.PutUint16(buf[offset:offset+2], uint16(4+len(element)))
+	return offset + 2
+}
+
+func (lp *listPack) pop(count int) [][]byte {
+	count = min(count, lp.length())
+	offset := int(binary.BigEndian.Uint32(lp.data[:4])) - 1
+	elements := make([][]byte, count)
+	for i := count - 1; i >= 0; i-- {
+		// Start of total element size field
+		offset -= 2
+		elemSize := int(binary.BigEndian.Uint16(lp.data[offset : offset+2]))
+		// Copy element data
+		dataSize := elemSize - 4
+		element := make([]byte, dataSize)
+		copy(element, lp.data[offset-dataSize:offset])
+		elements[i] = element
+		// Start of element data
+		offset -= dataSize + 2
+	}
+	lp.data[offset] = 0xFF
+	offset += 1
+	length := lp.length() - count
+	binary.BigEndian.PutUint32(lp.data[0:4], uint32(offset))
+	binary.BigEndian.PutUint16(lp.data[4:6], uint16(length))
+	return elements
+}
---
+++ internal/storage/lists/memory/listPack_test.go
@@ -0,0 +1,48 @@
+package memory
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestListPack_NewEmptyListPack(t *testing.T) {
+	lp := newEmptyListPack()
+	assert.Equal(t, 0, lp.length())
+}
+
+func TestListPack_NewListPack(t *testing.T) {
+	elements := [][]byte{[]byte("hello"), []byte("world")}
+	lp := newListPack(elements...)
+	assert.Equal(t, 2, lp.length())
+}
+
+func TestListPack_PushElements(t *testing.T) {
+	initialElements := [][]byte{[]byte("hello"), []byte("world")}
+	lp := newListPack(initialElements...)
+
+	size := lp.push([][]byte{[]byte("avacado"), []byte("listPack")}...)
+
+	assert.Equal(t, 4, size)
+	assert.Equal(t, 4, lp.length())
+}
+
+func TestListsMemoryStore_PopElements(t *testing.T) {
+	initialElements := [][]byte{[]byte("hello"), []byte("world")}
+	lp := newListPack(initialElements...)
+	lp.push([][]byte{[]byte("avacado"), []byte("listPack")}...)
+
+	elements := lp.pop(1)
+	assert.Equal(t, []byte("listPack"), elements[0])
+	assert.Equal(t, 3, lp.length())
+
+	elements = lp.pop(2)
+	assert.Equal(t, []byte("world"), elements[0])
+	assert.Equal(t, []byte("avacado"), elements[1])
+	assert.Equal(t, 1, lp.length())
+
+	elements = lp.pop(3)
+	assert.Equal(t, 1, len(elements))
+	assert.Equal(t, []byte("hello"), elements[0])
+	assert.Equal(t, 0, lp.length())
+}
